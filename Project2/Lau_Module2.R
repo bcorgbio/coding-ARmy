7298221d245617aed9a011819ccacd06e03b076f
Justin Lau <laujm@bc.edu>
2022-09-18 14:55:45 -0700

    comments added

--- Project2/Lau_Module2.R
+++ Project2/Lau_Module2.R
@@ -30,25 +30,25 @@ find.peaks <- function(x,y,mult=100){
   return(f$peaks)
 }
 
-pseed.max <- pseed.wide%>%
+pseed.max <- pseed.wide%>% #determine peaks of amp.sum in pseed.wide by fish and speed
   group_by(fish,speed)%>%
   mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
   filter(peak==T)
 pseed.max$peak <- NULL
 
-pseed.sum.max<- pseed.max %>%
+pseed.sum.max<- pseed.max %>% #summarize mean of amp.sum by fish and speed
   group_by(fish, speed) %>%
   summarize(amp.sum.mean=mean(amp.sum)) 
   
 # 3.  Custom function to compute standard error of the mean (SE) --------
 SE <- function(x){
   sd(x)/ sqrt(length(x))
-}
+} #custom function
 
-pseed.sum.se <- pseed.max%>%
+pseed.sum.se <- pseed.max%>% #determine SE of mean of amp.sum using the pseed.max data set
   group_by(fish,speed)%>%
   summarize(amp.sum.se = SE(amp.sum))
-pseed.sum.max <- pseed.sum.max %>%
+pseed.sum.max <- pseed.sum.max %>% #join the SE with mean of amp.sum
   left_join(pseed.sum.se, by = c("speed","fish"))
 
 # 4. Plot mean amp.sum vs specific swimming speed -------------------------
@@ -58,12 +58,12 @@ pseed.sum.max %>%
 # 5. Merge met rate with pseed.sum.max --------------------------------------------
 pseed.met.rate <- read_csv("pseed.met.rate.csv")
 pseed.max <- pseed.max%>%
-  merge(pseed.met.rate,by=c("fish","date","m.s","cm.s","bl.s"))
+  merge(pseed.met.rate,by=c("fish","date","m.s","cm.s","bl.s")) # match metabolic rate to all the other columns of pseed.max and merge. Had to use merge instead of left_join because left_join() was returning NA values even though the column values matched
 pseed.mean.rate <- pseed.max %>%
   group_by(fish, speed)%>%
-  summarize(amp.met.rate=mean(met.rate))
+  summarize(amp.met.rate=mean(met.rate)) 
 pseed.sum.max <- pseed.sum.max %>%
-  left_join(pseed.mean.rate, by = c("speed","fish"))
+  left_join(pseed.mean.rate, by = c("speed","fish")) 
 
 # 6. plot  metabolic power output vs. amp.sum.mean --------
 pseed.sum.max %>%
