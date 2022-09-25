6dc165445a0104c55a8bbcfe54468c62e277bb0b
Justin Lau <laujm@bc.edu>
2022-09-25 13:23:41 -0700

    Sorta Finished?

--- Project3/Coding_aRmy_Module3.R
+++ Project3/Coding_aRmy_Module3.R
@@ -25,32 +25,28 @@
     mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
 
 # 2. Construct two simple linear models that assess the effect of  perch height and diameter--------
-  # Perch Height
+# Perch Height
   anole.log.ph.lm <- lm(HTotal~SVL+PH, anole.log)
-  
-  # Perch Diameter
+
+# Perch Diameter
   anole.log.pd.lm <- lm(HTotal~SVL+ArbPD, anole.log)
 
 # 3. Explore how perch diameter and height affects Hindlimb-SVL relationship --------
-  # Add Residuals
+# Add Residuals
   anole.log <- anole.log %>%
     mutate(res.ph = residuals(anole.log.ph.lm), 
-           res.pd = residuals(anole.log.pd.lm))
-  
-  # Effect of Perch Height on Hindlimb-SVL Pattern
+          res.pd = residuals(anole.log.pd.lm))
+
+# Effect of Perch Height on Hindlimb-SVL Pattern
   anole.log%>%
-    ggplot(aes(x=PH,y=res.ph)) +geom_boxplot() 
-  # Effect of Perch Diameter on Hindlimb-SVL Pattern
+    ggplot(aes(x=PH,y=res.ph)) + geom_boxplot() 
+# Effect of Perch Diameter on Hindlimb-SVL Pattern
   anole.log%>%
-    ggplot(aes(x=ArbPD,y=res.pd)) +geom_boxplot() 
+    ggplot(aes(x=ArbPD,y=res.pd)) + geom_boxplot() 
 
 # 4. Use a BM model of trait evolution and construct phylogenetic least squares models --------
   anole.tree <- read.tree("anole.tre")
 
-  pgls.BM <- gls(HTotal~SVL, 
-                 correlation = corBrownian(1, phy = anole.tree, form =~Species), 
-                 data= anole.log, method = 'ML')
-  
   # A PGLS model with the hindlimb-SVL relationship + perch height
   pgls.BM.ph <- gls(HTotal~SVL + PH, 
                     correlation = corBrownian(1, phy = anole.tree, form =~Species), 
@@ -67,27 +63,26 @@
                       data= anole.log, method = 'ML')
 
 # 5.  Assess the fit of each of the three models using AICc and AICw --------
-  
   anole.phylo.aic <- AICc(pgls.BM, pgls.BM.ph, pgls.BM.pd, pgls.BM.ph.pd)
   aicw(anole.phylo.aic$AICc)
-  
+
   # fit     delta           w
   # 1 -63.66118 11.864530 0.001852884
   # 2 -64.77956 10.746149 0.003241168
   # 3 -73.81081  1.714901 0.296354947
   # 4 -75.52571  0.000000 0.698551002
-  
+
   # a phylogenetically corrected regression model that includes 
   # Perch Height and Diameter with traits evolving under BM is the best fit.
   # Both of the covariates together are a significant predictor of hindlimb 
   # length in a phylogenetic context. 
-  
+
 
 # 6. Produce a plot of your own design to concisely visualize effe --------
-  #Mutate anole.log to include phylogenetically best fit
+#Mutate anole.log to include phylogenetically best fit
   anole.log <- anole.log %>%
     mutate(phylo.res = residuals(pgls.BM.ph.pd))
-  
+
   # Facet Grid Plot of PH
   p.ph.phylo <- anole.log %>%
     dplyr::select(PH,res.ph,phylo.res) %>%
@@ -104,14 +99,14 @@
     print%>%
     ggplot(aes(x = ArbPD,y = value)) + 
     geom_boxplot() +
-    facet_grid(name~.,scales = "free_y") + ylab("residual")
+  facet_grid(name~.,scales = "free_y") + ylab("residual")
 
   # Plot both Facet Grids next to each other using cowplot package
   plot_grid(p.ph.phylo,p.pd.phylo, labels = "AUTO")
-  
-  
 
-  
+
+
+
 
 
 
